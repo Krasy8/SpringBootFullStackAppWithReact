474969d26292667c7f61681a3e7037cdd0c1def4
Konrad_Krasowski <krasy8@gmail.com>
2021-05-24 10:50:17 +0100

    snowplow tracker moved from controller to studentService

--- src/main/java/com/krasy8/fullStackApp/student/StudentController.java
+++ src/main/java/com/krasy8/fullStackApp/student/StudentController.java
@@ -4,9 +4,6 @@ import com.krasy8.fullStackApp.spTracking.ReqCallBack;
 import com.snowplowanalytics.snowplow.tracker.DevicePlatform;
 import com.snowplowanalytics.snowplow.tracker.Tracker;
 import com.snowplowanalytics.snowplow.tracker.emitter.BatchEmitter;
-import com.snowplowanalytics.snowplow.tracker.emitter.Emitter;
-import com.snowplowanalytics.snowplow.tracker.events.PageView;
-import com.snowplowanalytics.snowplow.tracker.events.Timing;
 import com.snowplowanalytics.snowplow.tracker.events.Unstructured;
 import com.snowplowanalytics.snowplow.tracker.payload.SelfDescribingJson;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -54,47 +51,6 @@ public class StudentController {
 
     @DeleteMapping("{studentId}")
     public void deleteStudent(@PathVariable("studentId") UUID studentId) {
-
-        trackDeletedAccount(studentId);
-
         studentService.deleteStudent(studentId);
-    }
-
-    private void trackDeletedAccount(UUID studentId) {
-
-        String collectorEndpoint = "http://0.0.0.0:9090";
-        String appId = "sp-java-tracker-backend";
-        String namespace = "demo";
-
-        BatchEmitter emitter = BatchEmitter.builder()
-                .url(collectorEndpoint)
-                .requestCallback(new ReqCallBack())
-                .bufferSize(5)
-                .build();
-
-        final Tracker tracker = new Tracker.TrackerBuilder(emitter, namespace, appId)
-                .base64(true)
-                .platform(DevicePlatform.ServerSideApp)
-                .build();
-
-        Student studentToDelete = studentService.findStudent(studentId);
-
-        Map<String, Object> eventMap = new HashMap<>();
-        eventMap.put("userFirstName", studentToDelete.getFirstName());
-        eventMap.put("userLastName", studentToDelete.getLastName());
-        eventMap.put("userEmail", studentToDelete.getEmail());
-        eventMap.put("leavingDate", LocalDate.now().toString());
-        eventMap.put("leavingTime", LocalTime.now().toString());
-
-        Unstructured unstructured = Unstructured.builder()
-                .eventData(new SelfDescribingJson(
-                        "iglu:com.snowplowanalytics.iglu/anything-a/jsonschema/1-0-0",
-                        eventMap
-                ))
-                .build();
-
-        tracker.track(unstructured);
-
-        emitter.close();
     }
 }
--- src/main/java/com/krasy8/fullStackApp/student/StudentService.java
+++ src/main/java/com/krasy8/fullStackApp/student/StudentService.java
@@ -2,13 +2,19 @@ package com.krasy8.fullStackApp.student;
 
 import com.krasy8.fullStackApp.EmailValidator;
 import com.krasy8.fullStackApp.exception.ApiRequestException;
+import com.krasy8.fullStackApp.spTracking.ReqCallBack;
+import com.snowplowanalytics.snowplow.tracker.DevicePlatform;
+import com.snowplowanalytics.snowplow.tracker.Tracker;
+import com.snowplowanalytics.snowplow.tracker.emitter.BatchEmitter;
+import com.snowplowanalytics.snowplow.tracker.events.Unstructured;
+import com.snowplowanalytics.snowplow.tracker.payload.SelfDescribingJson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.*;
 
 @Service
 public class StudentService {
@@ -26,6 +32,10 @@ public class StudentService {
         return studentDataAccessService.selectAllStudents();
     }
 
+    public Student findStudent(UUID studentId) {
+        return studentDataAccessService.findStudentById(studentId);
+    }
+
     void addNewStudent(Student student) {
         addNewStudent(null, student);
     }
@@ -33,9 +43,9 @@ public class StudentService {
     public void addNewStudent(UUID studentId, Student student) {
         UUID newStudentId = Optional.ofNullable(studentId).orElse(UUID.randomUUID());
 
-            if (!emailValidator.test(student.getEmail())) {
-                throw new ApiRequestException(student.getEmail() + " is not a valid email address");
-            }
+        if (!emailValidator.test(student.getEmail())) {
+            throw new ApiRequestException(student.getEmail() + " is not a valid email address");
+        }
         // TODO: Verify that email is not taken
 
         if (studentDataAccessService.isEmailTaken(student.getEmail())) {
@@ -74,12 +84,55 @@ public class StudentService {
 
     void deleteStudent(UUID studentId) {
 
+        // prepare the event to track
+        Unstructured deletedStudentEvent = buildDeletedStudentEvent(studentId);
 
-
+        // delete the student
         studentDataAccessService.deleteStudentById(studentId);
+
+        //send the event
+        sendDeletedStudentEvent(deletedStudentEvent);
+
     }
 
-    public Student findStudent(UUID studentId) {
-       return studentDataAccessService.findStudentById(studentId);
+    private Unstructured buildDeletedStudentEvent(UUID studentId) {
+
+        Student studentToDelete = findStudent(studentId);
+
+        Map<String, Object> eventMap = new HashMap<>();
+        eventMap.put("userFirstName", studentToDelete.getFirstName());
+        eventMap.put("userLastName", studentToDelete.getLastName());
+        eventMap.put("userEmail", studentToDelete.getEmail());
+        eventMap.put("leavingDate", LocalDate.now().toString());
+        eventMap.put("leavingTime", LocalTime.now().toString());
+
+        return Unstructured.builder()
+                .eventData(new SelfDescribingJson(
+                        "iglu:com.snowplowanalytics.iglu/anything-a/jsonschema/1-0-0",
+                        eventMap
+                ))
+                .build();
+    }
+
+    private void sendDeletedStudentEvent(Unstructured deletedStudentEvent) {
+
+        String collectorEndpoint = "http://0.0.0.0:9090";
+        String appId = "sp-java-tracker-backend";
+        String namespace = "demo";
+
+        BatchEmitter emitter = BatchEmitter.builder()
+                .url(collectorEndpoint)
+                .requestCallback(new ReqCallBack())
+                .bufferSize(5)
+                .build();
+
+        final Tracker tracker = new Tracker.TrackerBuilder(emitter, namespace, appId)
+                .base64(true)
+                .platform(DevicePlatform.ServerSideApp)
+                .build();
+
+        tracker.track(deletedStudentEvent);
+
+        emitter.close();
     }
 }
